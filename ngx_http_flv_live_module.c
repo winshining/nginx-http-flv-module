bf93d2c7cf7026b561553333d828e5bcfe42647d
syklevin <syk.levin@gmail.com>
2017-12-15 17:01:42 +0800

    add cors header

--- ngx_http_flv_live_module.c
+++ ngx_http_flv_live_module.c
@@ -235,6 +235,8 @@ ngx_http_flv_live_send_header(ngx_rtmp_session_t *s)
         CRLF
         "Cache-Control: no-cache"
         CRLF
+        "Access-Control-Allow-Origin: *"
+        CRLF
         "Transfer-Encoding: chunked"
         CRLF
         CRLF);
@@ -247,6 +249,8 @@ ngx_http_flv_live_send_header(ngx_rtmp_session_t *s)
         "Connection: keep-alive"
         CRLF
         "Cache-Control: no-cache"
+        CRLF
+        "Access-Control-Allow-Origin: *"
         CRLF
         "Expires: -1"
         CRLF
