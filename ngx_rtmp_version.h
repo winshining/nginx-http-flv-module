e20f1deaa61f4215115ac5c00524ede42737f984
plainheart <yhen@all-my-life.cn>
2018-06-29 09:17:38 +0800

    [misc] merge some changes from master

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -9,8 +9,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1002003
-#define NGINX_RTMP_VERSION  "1.2.3"
+#define nginx_rtmp_version  1002004
+#define NGINX_RTMP_VERSION  "1.2.4"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
