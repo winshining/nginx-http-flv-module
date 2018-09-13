317d1cc7623dd7630823c5aa58c489f335840733
winshining <winshining@163.com>
2018-09-14 01:35:30 +0800

    [misc] fix a potential crash bug, remove unused codes and update version.

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -9,8 +9,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1002004
-#define NGINX_RTMP_VERSION  "1.2.4"
+#define nginx_rtmp_version  1002005
+#define NGINX_RTMP_VERSION  "1.2.5"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
