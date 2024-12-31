2e840c2e336137cd4da43ff6ccfa90a32f9091e0
winshining <winshining@163.com>
2024-12-31 18:58:15 +0800

    [dev] version bump.

--- ngx_rtmp_version.h
+++ ngx_rtmp_version.h
@@ -9,8 +9,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1002011
-#define NGINX_RTMP_VERSION  "1.2.11"
+#define nginx_rtmp_version  1002012
+#define NGINX_RTMP_VERSION  "1.2.12"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
