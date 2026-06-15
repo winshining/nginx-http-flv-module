winshining/nginx-http-flv-module

commit f855fd8748d0d84b79f47f76e05af4a41f58a540

[fix] fixed a crash bug.
If server received an HTTP/1.0 request without a header `Host`,
`r->headers_in.host` would be NULL, then a worker process would
crash because of dereferencing `r->headers_in.host->value`.
Spotted by shwan@chtsecurity.com, thanks.

ngx_http_flv_live_module.c
@@ -323,7 +323,7 @@ ngx_http_flv_live_init_handlers(ngx_cycle_t *cycle)
         return NGX_OK;
     }
 
-    /* rtmp live conf aready exsits, so add additional event handlers */
+    /* rtmp live conf already exsits, so add additional event handlers */
     h = ngx_array_push(&cmcf->events[NGX_HTTP_FLV_LIVE_REQUEST]);
     *h = ngx_http_flv_live_request;
 
@@ -2064,6 +2064,7 @@ ngx_http_flv_live_connect_init(ngx_rtmp_session_t *s, ngx_str_t *app,
     ngx_connection_t            *c;
     ngx_http_request_t          *r;
     u_char                       name[NGX_RTMP_MAX_NAME];
+    ngx_str_t                    addr;
 
     r = s->data;
     c = s->connection;
@@ -2074,8 +2075,17 @@ ngx_http_flv_live_connect_init(ngx_rtmp_session_t *s, ngx_str_t *app,
     ngx_memcpy(v.args, r->args.data, ngx_min(r->args.len, sizeof(v.args) - 1));
     ngx_memcpy(v.flashver, "flv_live 1.1", ngx_strlen("flv_live 1.1"));
 
-    *ngx_snprintf(v.tc_url, NGX_RTMP_MAX_URL, "http://%V/%V",
-            &r->headers_in.host->value, app) = 0;
+    if (r->headers_in.host) {
+        *ngx_snprintf(v.tc_url, NGX_RTMP_MAX_URL, "http://%V/%V",
+                      &r->headers_in.host->value, app) = 0;
+    } else {
+        if (ngx_connection_local_sockaddr(c, &addr, 1) != NGX_OK) {
+            return NGX_ERROR;
+        }
+
+        *ngx_snprintf(v.tc_url, NGX_RTMP_MAX_URL, "http://%V/%V",
+                      &addr, app) = 0;
+    }
 
 #define NGX_RTMP_SET_STRPAR(name)                                          \
     s->name.len = ngx_strlen(v.name);                                      \

ngx_rtmp_version.h
@@ -9,8 +9,8 @@
 #define _NGX_RTMP_VERSION_H_INCLUDED_
 
 
-#define nginx_rtmp_version  1002013
-#define NGINX_RTMP_VERSION  "1.2.13"
+#define nginx_rtmp_version  1002014
+#define NGINX_RTMP_VERSION  "1.2.14"
 
 
 #endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */

commit 6a1e23558ff2edb58d1e354420c0b2a20f6f7599

[fix] fixed a potential crash bug introduced by f855fd8748d0.

ngx_http_flv_live_module.c
@@ -2065,6 +2065,7 @@ ngx_http_flv_live_connect_init(ngx_rtmp_session_t *s, ngx_str_t *app,
     ngx_http_request_t          *r;
     u_char                       name[NGX_RTMP_MAX_NAME];
     ngx_str_t                    addr;
+    u_char                       addr_buf[NGX_SOCKADDR_STRLEN];
 
     r = s->data;
     c = s->connection;
@@ -2079,6 +2080,9 @@ ngx_http_flv_live_connect_init(ngx_rtmp_session_t *s, ngx_str_t *app,
         *ngx_snprintf(v.tc_url, NGX_RTMP_MAX_URL - 1, "http://%V/%V",
                       &r->headers_in.host->value, app) = 0;
     } else {
+        addr.len = sizeof(addr_buf);
+	addr.data = addr_buf;
+
         if (ngx_connection_local_sockaddr(c, &addr, 1) != NGX_OK) {
             return NGX_ERROR;
         }